4996397334ffcca88892f457e55274deee39bd5c
Milosz Dudzik <miloszdudzik@gmail.com>
2025-03-17 20:55:08 +0100

    Adding webDriverFactory to create driver for any browser with specified options from browser configs

---
+++ src/main/java/com/drivers/WebDriverFactory.java
@@ -0,0 +1,32 @@
+package com.drivers;
+
+import com.config.*;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class WebDriverFactory {
+
+    public static WebDriver createDriver(Browser browser, BrowserOptions options) {
+
+        BrowserConfigurator configurator = getConfigurator(browser);
+        MutableCapabilities capabilities = configurator.configure(options);
+
+        return switch (browser) {
+            case EDGE -> new EdgeDriver((EdgeOptions) capabilities);
+            case FIREFOX -> new FirefoxDriver((FirefoxOptions) capabilities);
+            default -> throw new IllegalArgumentException("Unsupported browser: " + browser);
+        };
+    }
+
+    private static BrowserConfigurator getConfigurator(Browser browser) {
+        return switch (browser) {
+            case EDGE -> new EdgeConfigurator();
+            case FIREFOX -> new FirefoxConfigurator();
+            default -> throw new IllegalArgumentException("Unsupported browser: " + browser);
+        };
+    }
+}
