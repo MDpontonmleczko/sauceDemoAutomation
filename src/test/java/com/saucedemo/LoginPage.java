1e5c53a60690dd6283b07a6bcbb309cfd3ed805d
Milosz Dudzik <miloszdudzik@gmail.com>
2025-03-20 22:12:24 +0100

    Updated methods to use wait methods from AbstractPage.java

--- src/test/java/com/saucedemo/LoginPage.java
+++ src/test/java/com/saucedemo/LoginPage.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class LoginPage extends AbstractPage{
     private final static String LOGIN_PAGE_URL = "https://www.saucedemo.com/";
@@ -31,33 +30,35 @@ public class LoginPage extends AbstractPage{
     }
 
     public LoginPage setUsername(String username) {
-        wait.until(ExpectedConditions.visibilityOf(usernameFieldLocator)).sendKeys(username);
+        waitUntilElementIsClickable(usernameFieldLocator).sendKeys(username);
         return this;
     }
 
     public LoginPage setPassword(String password) {
-        wait.until(ExpectedConditions.visibilityOf(passwordFieldLocator)).sendKeys(password);
+        waitUntilElementIsClickable(passwordFieldLocator).sendKeys(password);
         return this;
     }
 
     public LoginPage clearUsername() {
+        waitUntilElementIsClickable(usernameFieldLocator);
         usernameFieldLocator.sendKeys(Keys.CONTROL + "a");
         usernameFieldLocator.sendKeys(Keys.DELETE);
         return this;
     }
 
     public LoginPage clearPassword() {
+        waitUntilElementIsClickable(passwordFieldLocator);
         passwordFieldLocator.sendKeys(Keys.CONTROL + "a");
         passwordFieldLocator.sendKeys(Keys.DELETE);
         return this;
     }
 
-    public LoginPage clickLoginButton() {
-        wait.until(ExpectedConditions.visibilityOf(loginButtonLocator)).click();
-        return this;
+    public MainPage clickLoginButton() {
+        waitUntilElementIsClickable(loginButtonLocator).click();
+        return new MainPage(driver);
     }
 
     public String getErrorMessage() {
-        return wait.until(ExpectedConditions.visibilityOf(errorMessageLocator)).getText();
+        return waitUntilElementIsVisible(errorMessageLocator).getText();
     }
 }
