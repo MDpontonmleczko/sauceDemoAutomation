c0e766ccd349e7bc8b1c2afcc49051202b2ddd73
Milosz Dudzik <miloszdudzik@gmail.com>
2025-03-20 14:14:07 +0100

    Updating method shouldReturnErrorMessageWhenLoginCredentialsAreEmpty by adding User object, changing assertion to equals and changing name to shouldReturnErrorMessageWhenLoginCredentialsAreDeleted

--- src/test/java/com/test/TestLoginPage.java
+++ src/test/java/com/test/TestLoginPage.java
@@ -1,25 +1,32 @@
 package com.test;
 
+import com.model.User;
 import com.saucedemo.LoginPage;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
-public class TestLoginPage {
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestLoginPage extends CommonConditions{
 
     @Test()
-    public void shouldReturnErrorMessageWhenLoginCredentialsAreEmpty() {
+    public void shouldReturnErrorMessageWhenLoginCredentialsAreDeleted() {
 
+        //given
+        User user = new User("username", "password");
         String expectedErrorMessage = "Username is required";
-        String actualErrorMessage = LoginPage
+
+        //when
+        String actualErrorMessage = new LoginPage(super.driver)
                 .open()
-                .setUsername(username)
-                .setPassword(password)
+                .setUsername(user.username())
+                .setPassword(user.password())
                 .clearUsername()
                 .clearPassword()
-                .clicLoginButton()
+                .clickLoginButton()
                 .getErrorMessage();
 
-        assertEquals(expectedErrorMessage, actualErrorMessage,
+        //then
+        assertTrue(actualErrorMessage.contains(expectedErrorMessage),
                 "Expected: '" + expectedErrorMessage +  "' Instead got: '" + actualErrorMessage + "'");
     }
 }
