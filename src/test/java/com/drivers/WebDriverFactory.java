1ed0aee480be106d0ebad544befefe401986452b
Milosz Dudzik <miloszdudzik@gmail.com>
2025-03-21 17:34:30 +0100

    Added pageLoadTimeout to driver and set for 60 sec

--- src/test/java/com/drivers/WebDriverFactory.java
+++ src/test/java/com/drivers/WebDriverFactory.java
@@ -8,17 +8,22 @@ import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
+import java.time.Duration;
+
 public class WebDriverFactory {
 
     public static WebDriver createDriver(Browser browser, BrowserOptions options) {
         BrowserConfigurator configurator = getConfigurator(browser);
         MutableCapabilities capabilities = configurator.configure(options);
 
-        return switch (browser) {
+        WebDriver driver = switch (browser) {
             case EDGE -> new EdgeDriver((EdgeOptions) capabilities);
             case FIREFOX -> new FirefoxDriver((FirefoxOptions) capabilities);
             default -> throw new IllegalArgumentException("Unsupported browser: " + browser);
         };
+
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+        return driver;
     }
 
     private static BrowserConfigurator getConfigurator(Browser browser) {
